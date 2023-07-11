9187eefe54a725f72a238b5111319e8ac01026c8
gablisb0a <95422158+gablisb0a@users.noreply.github.com>
2023-07-11 09:02:05 -0300

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
 
 
-  <Sidebar v-model="visible">
-    <h2>Test</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <Sidebar v-model:visible="visible">
+    
   </Sidebar>
 
 
@@ -12,9 +11,9 @@
     <template #start>
     
     <div class="flex items-center content-center gap-4">
-          <Button outlined severity="secondary" icon="pi pi-bars" @click="visible = true"/>
+          <Button size="small" outlined severity="secondary" icon="pi pi-bars" @click="visible = true"/>
 
-        <a class="flex items-center self-center" href="">
+        <a style="align-self: center" class="flex items-center self-center" href="">
         <svg  width="82" height="16" viewBox="0 0 82 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M16.6613 0L0.447256 13.6503L0 16H2.33599L14.8958 5.43151L12.8713 16H15.9351L19 0H16.6613Z" fill="#F3652B"/>
               <path fill-rule="evenodd" clip-rule="evenodd" d="M22.0645 0L21.4854 3.02307H32.0678L19.4534 13.6444L19 16H34.9355L35.5146 12.9769H24.9323L37.5495 2.3533L38 0H22.0645Z" fill="#F3652B"/>
@@ -27,10 +26,14 @@
          
 
 
+
     </template>
     <template #end>
-        <InputText placeholder="Search" type="text" />
-        
+      <div class="flex flex-wrap gap-2">
+            <Button raised size="small" label="Create Application" icon="pi pi-plus-circle" iconPos="right"/>
+            <Button size="small" outlined severity="secondary" icon="pi pi-search" ></Button>
+            <Avatar v-badge="4" icon="pi pi-user" size="xlarge" />
+        </div>
     </template>
 </Menubar>
 
@@ -38,36 +41,30 @@
 <!-- Banner App -->
 <section class="flex-col p-8">
     <div class="">
-        <div class="self-stretch text-gray-700 text-2xl font-normal">Welcome, <span>UserName</span> </div>
-        <h1 class="self-stretch text-gray-900 text-4xl font-bold">Build powerful applications using Edge Computing.</h1>
+        <div class="self-stretch text-gray-200 text-2xl font-normal">Welcome, {{userName}} </div>
+        <h1 class="self-stretch text-4xl font-bold">Build powerful applications using Edge Computing.</h1>
     </div>
 
     <div class="flex flex-wrap gap-2">
-          <Button size="small" label="Create Application" icon="pi pi-plus-circle" iconPos="right"/>
+          <Button raised size="small" label="Create Application" icon="pi pi-plus-circle" iconPos="right"/>
           <Button size="small" label="Documentation" icon="pi pi-book" plain text />
           <Button size="small" label="Azion API" icon="pi pi-code" plain text />
     </div>
 </section>
 
-
+<!-- Section Metrics -->
 <section class="flex-col gap-4">
 
   <div class="p-8 flex align-items-center gap-4">
-      <div class="text-gray-900 text-xl font-semibold">Real Time Metrics</div>
+      <div class="text-white text-xl font-semibold">Real Time Metrics</div>
       <div class="flex gap-2">
-      <Button size="small" label="Invite Member" icon="pi pi-chevron-right" plain text iconPos="right" /> </div>
-  </div>
-
-  <div class="p-8 flex align-items-center gap-4">
-      <div class="text-gray-900 text-xl font-semibold">Real Time Metrics</div>
-      <div class="flex gap-2">
-      <Button size="small" label="Invite Member" icon="pi pi-chevron-right" plain text iconPos="right" /> </div>
+      <Button size="small" label="See all Dashbords" icon="pi pi-chevron-right" plain text iconPos="right" /> </div>
   </div>
 </section>
 
-
+<!-- Section Teams -->
 <div class="p-8 flex align-items-center gap-4">
-    <div class="text-gray-900 text-xl font-semibold">Teams</div>
+    <div class="text-xl font-semibold">Teams</div>
     <div class="flex gap-2">
     <Button size="small" label="Invite Member" icon="pi pi-user-plus" plain text iconPos="right" /> </div>
 </div>
@@ -79,9 +76,11 @@
 <script setup>
 import { ref } from 'vue';
 import Button from 'primevue/button';
+const visible = ref ('');
 const visibleLeft = ref(false);
 const visibleRight = ref(false);
 const visibleTop = ref(false);
 const visibleBottom = ref(false);
+const userName = ref ('userName');
 
 </script>
